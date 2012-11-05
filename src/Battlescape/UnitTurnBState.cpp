f18e2bf75507c2b6e00ad52792e21dc027c1e516
Warboy <neo_nihilist@hotmail.com>
2012-11-05 14:48:37 +1100

    missed a spot

--- src/Battlescape/UnitTurnBState.cpp
+++ src/Battlescape/UnitTurnBState.cpp
@@ -60,11 +60,11 @@ void UnitTurnBState::init()
 		_parent->setStateInterval(Options::getInt("battleAlienSpeed"));
 
 	// if the unit has a turret and we are turning during targeting, then only the turret turns
-	turret = false;
+	_turret = false;
 	if (_action.actor->getMainHandWeapon())
-	turret = (_action.actor->getMainHandWeapon()->getRules()->getTurretType() != -1) && _action.targeting;
+	_turret = (_action.actor->getMainHandWeapon()->getRules()->getTurretType() != -1) && _action.targeting;
 
-	_unit->lookAt(_action.target, turret);
+	_unit->lookAt(_action.target, _turret);
 
 	if (_unit->getStatus() != STATUS_TURNING)
 	{
